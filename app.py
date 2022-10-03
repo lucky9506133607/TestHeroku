a470432df250828338ae793560f264244cf7382e
lucky9506133607 <lucky9506133607@gmail.com>
2022-10-03 22:30:41 +0530

    added: try block

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--headless")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-sh-usage")
-driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
+try:
+    print('in try block')
+    driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
+except Exception as e:
+    print("Exception arrived", e)
 driver.get("https://dev-veneta.myshopify.com/")
 print(driver.title)
 driver.quit()
