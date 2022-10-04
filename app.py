39d341ceb0f1454bb9c6ec5b010bfa467a9e3e57
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-04 10:04:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-def Test():        
-    op = webdriver.ChromeOptions()
-    op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    op.add_argument("--headless")
-    op.add_argument("--no-sandbox")
-    op.add_argument("--disable-dev-sh-usage")
-    try:
-        print('in try block')
-        driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
-    except Exception as e:
-        print("Exception arrived", e)
-    driver.get("https://dev-veneta.myshopify.com/")
-    print(driver.title)
-    driver.quit()
-
-__name__=="__main__"
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
+try:
+    print('in try block')
+    driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
+except Exception as e:
+    print("Exception arrived", e)
+driver.get("https://dev-veneta.myshopify.com/")
+print(driver.title)
+driver.quit()
