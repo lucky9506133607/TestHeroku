969fad041249d178309c88c6aaa1a0a6871c6edb
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-04 14:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import os
-op = webdriver.ChromeOptions()
-op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-op.add_argument("--headless")
-op.add_argument("--no-sandbox")
-op.add_argument("--disable-dev-sh-usage")
-try:
-    print('in try block')
-    driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
-except Exception as e:
-    print("Exception arrived", e)
-driver.get("https://venetablinds.com.au/")
-print(driver.title)
-driver.quit()
+def Test():
+	op = webdriver.ChromeOptions()
+	op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	op.add_argument("--headless")
+	op.add_argument("--no-sandbox")
+	op.add_argument("--disable-dev-sh-usage")
+	try:
+	    print('in try block')
+	    driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
+	except Exception as e:
+	    print("Exception arrived", e)
+	driver.get("https://venetablinds.com.au/")
+	print(driver.title)
+	driver.quit()
+
+if __name__ == '__main__':
+	Test()
