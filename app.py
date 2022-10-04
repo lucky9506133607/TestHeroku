d6a224f3897c528f8478ebc8a470e993af59b76d
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-04 09:54:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-def Test():
-        
-    op = webdriver.ChromeOptions()
-    op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    op.add_argument("--headless")
-    op.add_argument("--no-sandbox")
-    op.add_argument("--disable-dev-sh-usage")
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
+try:
+    print('in try block')
     driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=op)
-    driver.get("https://youtube.com")
-    print(driver.title)
-    driver.quit()
-
-__name__(Test())
-
+except Exception as e:
+    print("Exception arrived", e)
+driver.get("https://dev-veneta.myshopify.com/")
+print(driver.title)
+driver.quit()
 
 
 
